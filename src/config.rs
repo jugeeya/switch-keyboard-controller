1900d04d4cead25cf0d0268ac64c4bb4d4c96b66
jugeeya <jugeeya@llive.com>
2022-11-06 13:19:00 -0800

    Initial commit

---
+++ src/config.rs
@@ -0,0 +1,211 @@
+#![allow(non_snake_case)]
+#[allow(dead_code)]
+
+use skyline::libc::mkdir;
+use std::fs;
+use serde::{Deserialize, Serialize};
+use strum::IntoEnumIterator;
+
+use crate::keyboard::KeyboardKey;
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct Config {
+    LSTICK_UP: Vec<String>,
+    LSTICK_LEFT: Vec<String>,
+    LSTICK_RIGHT: Vec<String>,
+    LSTICK_DOWN: Vec<String>,
+    LSTICK: Vec<String>,
+    RSTICK_UP: Vec<String>,
+    RSTICK_LEFT: Vec<String>,
+    RSTICK_RIGHT: Vec<String>,
+    RSTICK_DOWN: Vec<String>,
+    RSTICK: Vec<String>,
+    DUP: Vec<String>,
+    DLEFT: Vec<String>,
+    DRIGHT: Vec<String>,
+    DDOWN: Vec<String>,
+    A: Vec<String>,
+    B: Vec<String>,
+    X: Vec<String>,
+    Y: Vec<String>,
+    L: Vec<String>,
+    ZL: Vec<String>,
+    R: Vec<String>,
+    ZR: Vec<String>,
+    PLUS: Vec<String>,
+    MINUS: Vec<String>,
+    TILT1: Vec<String>,
+    TILT2: Vec<String>,
+    X1: Vec<String>,
+    X2: Vec<String>,
+    X3: Vec<String>,
+    Y1: Vec<String>,
+    Y2: Vec<String>,
+    Y3: Vec<String>,
+    pub TILT1_MODIFIER: f64,
+    pub TILT2_MODIFIER: f64,
+    pub X1_MODIFIER: f64,
+    pub X2_MODIFIER: f64,
+    pub X3_MODIFIER: f64,
+    pub Y1_MODIFIER: f64,
+    pub Y2_MODIFIER: f64,
+    pub Y3_MODIFIER: f64,
+}
+
+pub static mut CONFIG: Config = Config {
+    LSTICK_UP: vec![],
+    LSTICK_LEFT: vec![],
+    LSTICK_RIGHT: vec![],
+    LSTICK_DOWN: vec![],
+    LSTICK: vec![],
+    RSTICK_UP: vec![],
+    RSTICK_LEFT: vec![],
+    RSTICK_RIGHT: vec![],
+    RSTICK_DOWN: vec![],
+    RSTICK: vec![],
+    DUP: vec![],
+    DLEFT: vec![],
+    DRIGHT: vec![],
+    DDOWN: vec![],
+    A: vec![],
+    B: vec![],
+    X: vec![],
+    Y: vec![],
+    L: vec![],
+    ZL: vec![],
+    R: vec![],
+    ZR: vec![],
+    PLUS: vec![],
+    MINUS: vec![],
+    TILT1: vec![],
+    TILT2: vec![],
+    X1: vec![],
+    X2: vec![],
+    X3: vec![],
+    Y1: vec![],
+    Y2: vec![],
+    Y3: vec![],
+    TILT1_MODIFIER: 0.5,
+    TILT2_MODIFIER: 0.75,
+    X1_MODIFIER: 0.33,
+    X2_MODIFIER: 0.5,
+    X3_MODIFIER: 0.66,
+    Y1_MODIFIER: 0.33,
+    Y2_MODIFIER: 0.5,
+    Y3_MODIFIER: 0.66,
+};
+
+macro_rules! c_str {
+    ($l:tt) => {
+        [$l.as_bytes(), "\u{0}".as_bytes()].concat().as_ptr()
+    };
+}
+
+pub fn init() {
+    unsafe {
+        mkdir(c_str!("sd:/switchkeyboard/"), 777);
+    }
+    let config_path = "sd:/switchkeyboard/config.toml";
+    println!("[switch-keyboard-controller] Checking for config file in switchkeyboard/config.toml...");
+    if fs::metadata(config_path).is_ok() {
+        println!("[switch-keyboard-controller] Keyboard config found. Loading...");
+        let combo_conf = fs::read_to_string(&config_path).unwrap();
+        if validate_config(&combo_conf) {
+            save_config_from_toml(&combo_conf);
+        } else {
+            save_config_from_defaults();
+        }
+    } else {
+        println!("[switch-keyboard-controller] No previous button combo file found. Creating...");
+        fs::write(config_path, DEFAULT_CONFIG).expect("Failed to write button config conf file");
+        save_config_from_defaults();
+    }
+}
+
+pub fn validate_config(data: &str) -> bool {
+    let conf: Config = toml::from_str(data).unwrap();
+
+    let all_keyboard_keys : Vec<String> = KeyboardKey::iter()
+        .map(|key| key.to_string().to_uppercase())
+        .collect();
+    let mut bad_keys = vec![];
+    for (button, keys) in serde_json::to_value(&conf)
+        .unwrap()
+        .as_object()
+        .unwrap() {
+        if !keys.is_array() {
+            continue;
+        }
+
+        if !keys
+            .as_array()
+            .unwrap()
+            .iter()
+            .all(|key| all_keyboard_keys.contains(&key.as_str().unwrap().to_uppercase())) {
+            bad_keys.push(format!("{button} = {keys:#?}"));
+        }
+    }
+
+    if !bad_keys.is_empty() {
+        panic!("switch-keyboard-controller\nconfiguration is invalid! Bad keys: {:#?}", bad_keys);
+    }
+
+    true
+}
+
+pub fn save_config_from_defaults() {
+    save_config_from_toml(DEFAULT_CONFIG);
+}
+
+pub fn save_config_from_toml(data: &str) {
+    let conf: Config = toml::from_str(data).unwrap();
+    unsafe {
+        CONFIG = conf;
+    }
+}
+
+pub const DEFAULT_CONFIG: &'static str = r#"LSTICK_UP = ["W"]
+LSTICK_LEFT = ["A"]
+LSTICK_DOWN = ["S"]
+LSTICK_RIGHT = ["D"]
+LSTICK = []
+RSTICK_UP = ["G"]
+RSTICK_LEFT = ["V"]
+RSTICK_DOWN = ["B"]
+RSTICK_RIGHT = ["N"]
+RSTICK = []
+DUP = ["UPARROW"]
+DLEFT = ["LEFTARROW"]
+DDOWN = ["DOWNARROW"]
+DRIGHT = ["RIGHTARROW"]
+A = ["J"]
+B = ["K"]
+X = ["L"]
+Y = ["I"]
+L = ["Y"]
+ZL = ["U"]
+R = ["O"]
+ZR = ["P"]
+PLUS = ["RETURN"]
+MINUS = ["QUOTE"]
+TILT1 = ["LEFTSHIFT"]
+TILT2 = ["RIGHTSHIFT"]
+X1 = []
+X2 = []
+X3 = []
+Y1 = []
+Y2 = []
+Y3 = []
+TILT1_MODIFIER = 0.5
+TILT2_MODIFIER = 0.75
+X1_MODIFIER = 0.33
+X2_MODIFIER = 0.5
+X3_MODIFIER = 0.75
+Y1_MODIFIER = 0.33
+Y2_MODIFIER = 0.5
+Y3_MODIFIER = 0.75
+
+# Available Options:
+#
+# todo
+"#;
--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,216 @@
 #![feature(proc_macro_hygiene)]
 
-use skyline::{hook, install_hook};
+use skyline::nn::hid::{GetNpadStyleSet, NpadGcState};
+use strum::IntoEnumIterator;
 
-extern "C" fn test() -> u32 {
-    2
+mod keyboard;
+use crate::keyboard::{KeyboardKey, KeyboardState};
+use std::collections::HashMap;
+
+mod config;
+use crate::config::CONFIG;
+
+#[allow(improper_ctypes)]
+extern "C" {
+    fn add_nn_hid_hook(callback: fn(*mut NpadGcState, *const u32));
 }
 
-#[hook(replace = test)]
-fn test_replacement() -> u32 {
+pub unsafe fn p1_controller_id() -> u32 {
+    let min_controller_id = (0..8)
+        .filter(|i| GetNpadStyleSet(i as *const _).flags != 0)
+        .min()
+        .unwrap_or(0);
 
-    let original_test = original!();
-
-    let val = original_test();
-
-    println!("[override] original value: {}", val); // 2
-
-    val + 1
+    let handheld_id = 0x20;
+    if GetNpadStyleSet(&handheld_id as *const _).flags != 0 {
+        handheld_id
+    } else {
+        min_controller_id
+    }
 }
 
-#[skyline::main(name = "skyline_rs_template")]
+fn key_matches(
+    kbd_state: KeyboardState, 
+    keys: &serde_json::Value, 
+    keyboard_values: &HashMap<String, u32>) -> bool {
+    if !keys.is_array() {
+        return false;
+    }
+
+    keys.as_array()
+        .unwrap()
+        .iter()
+        .any(|key| {
+            let keyboard_value = keyboard_values.get(key.as_str().unwrap());
+            return keyboard_value.is_some() && kbd_state.is_key(*keyboard_value.unwrap() as i32);
+        })
+}
+
+pub fn handle_get_npad_state(state: *mut NpadGcState, controller_id: *const u32) {
+    unsafe {
+        if *controller_id != p1_controller_id() {
+            return;
+        }
+
+        let update_count = (*state).updateCount;
+        let attributes = (*state).Flags;
+        
+        let mut kbd_state = KeyboardState::new();
+        keyboard::GetKeyboardState(&mut kbd_state);
+
+        let keyboard_keys : std::collections::HashMap<String, u32> = KeyboardKey::iter()
+            .map(|key| (key.to_string().to_uppercase(), key as u32))
+            .collect();
+        let overall_button_config = serde_json::to_value(&CONFIG)
+            .unwrap();
+        let button_config = overall_button_config
+            .as_object()
+            .unwrap();
+        for (button, keys) in button_config {
+            if !key_matches(kbd_state, keys, &keyboard_keys) {
+                continue;
+            }
+                
+            let bit = match button.as_str() {
+                "A" => Some(0),              //  A
+                "B" => Some(1),              //  B
+                "X" => Some(2),              //  X
+                "Y" => Some(3),              //  Y
+                "LSTICK" => Some(4),         //  Left Stick Button
+                "RSTICK" => Some(5),         //  Right Stick Button
+                "L" => Some(6),              //  L
+                "R" => Some(7),              //  R
+                "ZL" => Some(8),             //  ZL
+                "ZR" => Some(9),             //  ZR
+                "PLUS" => Some(10),          //  Plus
+                "MINUS" => Some(11),         //  Minus
+                "DLEFT" => Some(12),         //  D-Pad Left
+                "DUP" => Some(13),           //  D-Pad Up
+                "DRIGHT" => Some(14),        //  D-Pad Right
+                "DDOWN" => Some(15),         //  D-Pad Down
+                "LSTICK_LEFT" => Some(16),   //  Left Stick Left
+                "LSTICK_UP" => Some(17),     //  Left Stick Up
+                "LSTICK_RIGHT" => Some(18),  //  Left Stick Right
+                "LSTICK_DOWN" => Some(19),   //  Left Stick Down
+                "RSTICK_LEFT" => Some(20),   //  Right Stick Left
+                "RSTICK_UP" => Some(21),     //  Right Stick Up
+                "RSTICK_RIGHT" => Some(22),  //  Right Stick Right
+                "RSTICK_DOWN" => Some(23),   //  Right Stick Down
+                "SL_LEFT" => Some(24),       //  SL on Left Joy-Con
+                "SR_LEFT" => Some(25),       //  SR on Left Joy-Con
+                "SL_RIGHT" => Some(26),      //  SL on Right Joy-Con
+                "SR_RIGHT" => Some(27),      //  SR on Right Joy-Con
+                _ => None
+            };
+            if bit.is_some() {
+                (*state).Buttons = (*state).Buttons | 2u64.pow(bit.unwrap());
+            }
+            let lstick_x = match button.as_str() {
+                "LSTICK_RIGHT" => Some(32767),  //  Left Stick Right,
+                "LSTICK_LEFT" => Some(-32767),   //  Left Stick Left
+                _ => None
+            };
+            let lstick_y = match button.as_str() {
+                "LSTICK_UP" => Some(32767),     //  Left Stick Up
+                "LSTICK_DOWN" => Some(-32767),   //  Left Stick Down
+                _ => None
+            };
+            let rstick_x = match button.as_str() {
+                "RSTICK_RIGHT" => Some(32767),  //  Right Stick Right
+                "RSTICK_LEFT" => Some(-32767),   //  Right Stick Left
+                _ => None
+            };
+            let rstick_y = match button.as_str() {
+                "RSTICK_UP" => Some(32767),     //  Right Stick Up
+                "RSTICK_DOWN" => Some(-32767),   //  Right Stick Down
+                _ => None
+            };
+            if lstick_x.is_some() { (*state).LStickX = lstick_x.unwrap(); }
+            if lstick_y.is_some() { (*state).LStickY = lstick_y.unwrap(); }
+            if rstick_x.is_some() { (*state).RStickX = rstick_x.unwrap(); }
+            if rstick_y.is_some() { (*state).RStickY = rstick_y.unwrap(); }
+        }
+
+        // Handle stick modifiers
+        let tilt_1_matches = key_matches(
+            kbd_state, button_config.get("TILT1").unwrap(), &keyboard_keys);
+        let tilt_2_matches = key_matches(
+            kbd_state, button_config.get("TILT2").unwrap(), &keyboard_keys);
+        let x1_matches = key_matches(
+            kbd_state, button_config.get("X1").unwrap(), &keyboard_keys);
+        let x2_matches = key_matches(
+            kbd_state, button_config.get("X2").unwrap(), &keyboard_keys);
+        let x3_matches = key_matches(
+            kbd_state, button_config.get("X3").unwrap(), &keyboard_keys);
+        let y1_matches = key_matches(
+            kbd_state, button_config.get("Y1").unwrap(), &keyboard_keys);
+        let y2_matches = key_matches(
+            kbd_state, button_config.get("Y2").unwrap(), &keyboard_keys);
+        let y3_matches = key_matches(
+            kbd_state, button_config.get("Y3").unwrap(), &keyboard_keys);
+
+        if x1_matches {
+            (*state).LStickX = ((*state).LStickX as f64 * CONFIG.X1_MODIFIER) as i32;
+        } else if x2_matches {
+            (*state).LStickX = ((*state).LStickX as f64 * CONFIG.X2_MODIFIER) as i32;
+        } else if x3_matches {
+            (*state).LStickX = ((*state).LStickX as f64 * CONFIG.X3_MODIFIER) as i32;
+        } else if tilt_1_matches {
+            (*state).LStickX = ((*state).LStickX as f64 * CONFIG.TILT1_MODIFIER) as i32;
+        } else if tilt_2_matches {
+            (*state).LStickX = ((*state).LStickX as f64 * CONFIG.TILT2_MODIFIER) as i32;
+        }
+
+        if y1_matches {
+            (*state).LStickY = ((*state).LStickY as f64 * CONFIG.Y1_MODIFIER) as i32;
+        } else if y2_matches {
+            (*state).LStickY = ((*state).LStickY as f64 * CONFIG.Y2_MODIFIER) as i32;
+        } else if y3_matches {
+            (*state).LStickY = ((*state).LStickY as f64 * CONFIG.Y3_MODIFIER) as i32;
+        } else if tilt_1_matches {
+            (*state).LStickY = ((*state).LStickY as f64 * CONFIG.TILT1_MODIFIER) as i32;
+        } else if tilt_2_matches {
+            (*state).LStickY = ((*state).LStickY as f64 * CONFIG.TILT2_MODIFIER) as i32;
+        }
+
+        (*state).updateCount = update_count;
+        (*state).Flags = attributes;
+    }
+}
+
+#[skyline::main(name = "switch-keyboard-controller")]
 pub fn main() {
-    println!("Hello from Skyline Rust Plugin!");
+    println!("Hello from switch-keyboard-controller");
 
-    install_hook!(test_replacement);
+    std::panic::set_hook(Box::new(|info| {
+        let location = info.location().unwrap();
 
-    let x = test();
+        let msg = match info.payload().downcast_ref::<&'static str>() {
+            Some(s) => *s,
+            None => match info.payload().downcast_ref::<String>() {
+                Some(s) => &s[..],
+                None => "Box<Any>",
+            },
+        };
 
-    println!("[main] test returned: {}", x); // 3
+        let err_msg = format!("thread has panicked at '{}', {}", msg, location);
+        skyline::error::show_error(
+            69,
+            "Skyline plugin has panicked! Please open the details and send a screenshot to the developer, then close the game.\n",
+            err_msg.as_str(),
+        );
+    }));
+
+    config::init();
+
+    unsafe {
+        keyboard::InitializeKeyboard();
+    }
+
+    unsafe {
+        if (add_nn_hid_hook as *const ()).is_null() {
+            panic!("The NN-HID hook plugin could not be found and is required to add NRO hooks. Make sure libnn_hid_hook.nro is installed.");
+        }
+        add_nn_hid_hook(handle_get_npad_state);
+    }
 }
